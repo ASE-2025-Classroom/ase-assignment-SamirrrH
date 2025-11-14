1fa9e1e63d322c04f963085e38bb3c7bc0b1f8b8
SamirrrH <S.hussain2356@student,leedsbeckett,ac,uk>
2025-11-14 01:54:07 +0000

    circle

---
+++ BOOSEapp1/BOOSEapp1/AppCanvas.cs
@@ -0,0 +1,142 @@
+﻿using BOOSE;
+using System;
+using System.Drawing;
+
+namespace BOOSEapp1
+{
+    internal class AppCanvas : ICanvas
+    {
+        private Bitmap canvasBitmap;
+        private Graphics g;
+        private int xPos, yPos; // pen position
+        private Pen pen;
+        private Brush brush;
+
+        public AppCanvas(int xsize, int ysize)
+        {
+            canvasBitmap = new Bitmap(xsize, ysize);
+            g = Graphics.FromImage(canvasBitmap);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            pen = new Pen(Color.Black);
+            brush = new SolidBrush(Color.Black);
+
+            xPos = 100;
+            yPos = 100;
+
+            Clear();
+        }
+
+        public int Xpos
+        {
+            get => xPos;
+            set => xPos = value;
+        }
+
+        public int Ypos
+        {
+            get => yPos;
+            set => yPos = value;
+        }
+
+        
+        public object PenColour
+        {
+            get => pen.Color;
+            set
+            {
+                if (value is Color c)
+                {
+                    pen.Color = c;
+                    brush = new SolidBrush(c);
+                }
+            }
+        }
+
+        public void Circle(int radius, bool filled)
+        {
+            
+            Rectangle rect = new Rectangle(Xpos, Ypos, radius * 2, radius * 2);
+
+            if (filled)
+                g.FillEllipse(brush, rect);
+            else
+                g.DrawEllipse(pen, rect);
+        }
+
+        public void Clear()
+        {
+            g.Clear(Color.White);
+        }
+
+        public void DrawTo(int x, int y)
+        {
+            g.DrawLine(pen, Xpos, Ypos, x, y);
+            Xpos = x;
+            Ypos = y;
+        }
+
+        public object getBitmap()
+        {
+            return canvasBitmap;
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            Xpos = x;
+            Ypos = y;
+        }
+
+        public void Rect(int width, int height, bool filled)
+        {
+            Rectangle rect = new Rectangle(Xpos, Ypos, width, height);
+
+            if (filled)
+                g.FillRectangle(brush, rect);
+            else
+                g.DrawRectangle(pen, rect);
+        }
+
+        public void Reset()
+        {
+            Xpos = 100;
+            Ypos = 100;
+        }
+
+        public void Set(int width, int height)
+        {
+            
+            canvasBitmap = new Bitmap(width, height);
+            g = Graphics.FromImage(canvasBitmap);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Clear();
+            Reset();
+        }
+
+        public void SetColour(int red, int green, int blue)
+        {
+            Color c = Color.FromArgb(red, green, blue);
+            pen.Color = c;
+            brush = new SolidBrush(c);
+        }
+
+        public void Tri(int width, int height)
+        {
+            
+            Point p1 = new Point(Xpos, Ypos);
+            Point p2 = new Point(Xpos + width, Ypos);
+            Point p3 = new Point(Xpos + width / 2, Ypos - height);
+
+            Point[] points = { p1, p2, p3 };
+            g.DrawPolygon(pen, points);
+        }
+
+        public void WriteText(string text)
+        {
+            using (Font font = SystemFonts.DefaultFont)
+            {
+                g.DrawString(text, font, brush, Xpos, Ypos);
+            }
+        }
+    }
+}
--- BOOSEapp1/BOOSEapp1/Form1.cs
+++ BOOSEapp1/BOOSEapp1/Form1.cs
@@ -1,14 +1,161 @@
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
 
-using BOOSE;
-using System.Diagnostics;
 namespace BOOSEapp1
 {
     public partial class Form1 : Form
     {
+        private AppCanvas canvas;
+        private TextBox txtProgram;
+        private Button btnRun;
+
         public Form1()
         {
             InitializeComponent();
-            Debug.WriteLine(AboutBOOSE.about());
+
+            
+            txtProgram = new TextBox
+            {
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                Font = new Font("Consolas", 10),
+                Left = 10,
+                Top = 10,
+                Width = 320,
+                Height = ClientSize.Height - 60,
+                Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left
+            };
+            Controls.Add(txtProgram);
+
+            
+            btnRun = new Button
+            {
+                Text = "Run",
+                Left = 10,
+                Top = ClientSize.Height - 40,
+                Width = 80,
+                Height = 30,
+                Anchor = AnchorStyles.Bottom | AnchorStyles.Left
+            };
+            btnRun.Click += (s, e) =>
+            {
+                RunScript(txtProgram.Text);
+                Invalidate();   
+            };
+            Controls.Add(btnRun);
+
+            
+            this.Paint += Form1_Paint;
+
+            
+            canvas = new AppCanvas(1000, 600);
+            canvas.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var bmp = (Bitmap)canvas.getBitmap();
+            if (bmp == null) return;
+
+            int left = txtProgram.Right + 20;
+            int top = 10;
+
+            e.Graphics.DrawImage(bmp, left, top);
+        }
+
+        private void RunScript(string src)
+        {
+            canvas.Clear();
+            canvas.Reset();
+
+            var lines = src.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            foreach (var raw in lines)
+            {
+                var line = raw.Trim();
+                if (line.Length == 0) continue;
+                if (line.StartsWith("*") || line.StartsWith("#")) continue;
+
+                int space = line.IndexOf(' ');
+                string cmd = (space < 0 ? line : line[..space]).ToLowerInvariant();
+                string args = (space < 0 ? "" : line[(space + 1)..]).Trim();
+
+                switch (cmd)
+                {
+                    case "moveto":
+                        {
+                            var (x, y) = Two(args);
+                            canvas.MoveTo(x, y);
+                            break;
+                        }
+                    case "drawto":
+                        {
+                            var (x, y) = Two(args);
+                            canvas.DrawTo(x, y);
+                            break;
+                        }
+                    case "pen":
+                    case "setcolour":
+                        {
+                            var (r, g, b) = Three(args);
+                            canvas.SetColour(r, g, b);
+                            break;
+                        }
+                    case "circle":
+                        {
+                            int r = One(args);
+                            canvas.Circle(r, false);
+                            break;
+                        }
+                    case "rect":
+                        {
+                            var (w, h) = Two(args);
+                            canvas.Rect(w, h, false);
+                            break;
+                        }
+                    case "tri":
+                        {
+                            var (w, h) = Two(args);
+                            canvas.Tri(w, h);
+                            break;
+                        }
+                    case "write":
+                        {
+                            canvas.WriteText(args.Trim().Trim('"'));
+                            break;
+                        }
+                    case "clear":
+                        canvas.Clear();
+                        break;
+
+                    case "reset":
+                        canvas.Reset();
+                        break;
+                }
+            }
+        }
+
+        private static int One(string s) => int.Parse(s.Trim());
+
+        private static (int, int) Two(string s)
+        {
+            var p = s.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(t => t.Trim())
+                     .ToArray();
+            return (int.Parse(p[0]), int.Parse(p[1]));
+        }
+
+        private static (int, int, int) Three(string s)
+        {
+            var p = s.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(t => t.Trim())
+                     .ToArray();
+            return (int.Parse(p[0]), int.Parse(p[1]), int.Parse(p[2]));
         }
     }
 }
