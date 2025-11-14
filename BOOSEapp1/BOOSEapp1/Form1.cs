c8c32ea9d22b0c878d6c2df327b04b1655fcd43e
SamirrrH <S.hussain2356@student,leedsbeckett,ac,uk>
2025-11-14 01:22:49 +0000

    test

---
+++ BOOSEapp1/BOOSEapp1/Form1.cs
@@ -0,0 +1,14 @@
+
+using BOOSE;
+using System.Diagnostics;
+namespace BOOSEapp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
+        }
+    }
+}
